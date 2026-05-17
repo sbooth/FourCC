c6ab033dcc43806da5ad61a945d183f1ab61627e
Stephen Booth <me@sbooth.org>
2026-05-17 17:27:41 -0600

    Update tests

--- Tests/FourCCTests/FourCCTests.swift
+++ Tests/FourCCTests/FourCCTests.swift
@@ -1,12 +1,14 @@
-import XCTest
+import Testing
 @testable import FourCC
 
-final class FourCCTests: XCTestCase {
-	func testString() throws {
-		XCTAssertEqual("test".fourCC, 0x74657374)
+@Suite struct FourCCTests {
+	@Test func string() throws {
+		#expect("test".fourCC == 0x74657374)
+		#expect("te\0\0".fourCC == 0x74650000)
 	}
 
-	func testUInt32() {
-		XCTAssertEqual(UInt32(0x74657374).fourCC, "test")
+	@Test func uInt32() {
+		#expect(UInt32(0x74657374).fourCC == "test")
+		#expect(UInt32(0x74650000).fourCC == "te\0\0")
 	}
 }
